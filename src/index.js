8012dbfabb07a386d8f1ba2d09548582e2e6b1ea
Rob Hess <hessro@oregonstate.edu>
2023-01-18 13:00:53 -0800

    Add initial code.

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import App from './HandlingEventsApp'
+// import App from './ControlledComponentsApp'
+// import App from './GeneralPurposeSelectApp'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
