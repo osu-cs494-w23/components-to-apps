87f308b941dfb455d3a9c01e2e014c11483a482b
Rob Hess <hessro@oregonstate.edu>
2023-01-23 16:58:07 -0800

    Add code from 1/23 lecture

--- src/GeneralPurposeSelectApp.js
+++ src/GeneralPurposeSelectApp.js
@@ -5,7 +5,8 @@ const starWarsOptions = {
   luke: "Luke Skywalker",
   leia: "Leia Organa",
   finn: "Finn (FN-2187)",
-  kylo: "Kylo Ren"
+  kylo: "Kylo Ren",
+  darth: "Darth Vader"
 }
 
 const petOptions = {
@@ -15,24 +16,83 @@ const petOptions = {
   iguana: "Iguana"
 }
 
-class App extends React.Component {
-  constructor(props) {
+class Dropdown extends React.Component {
+  constructor (props) {
     super(props)
-    this.state = {}
+    // this.state = {
+    //   value: Object.keys(this.props.options)[0]
+    // }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    // this.setState({
+    //   value: event.target.value
+    // })
+    this.props.handleChange(event.target.value)
   }
 
   render() {
+    // console.log("== selected value:", this.state.value)
     return (
-      <form>
-        <select>
-          <option value="rey">Rey</option>
-          <option value="luke">Luke Skywalker</option>
-          <option value="leia">Leia Organa</option>
-          <option value="finn">Finn (FN-2187)</option>
-          <option value="kylo">Kylo Renn</option>
-        </select>
-        <button type="submit">Submit</button>
-      </form>
+      <select value={this.props.value} onChange={this.handleChange}>
+        {Object.keys(this.props.options).map(key => (
+          <option key={key} value={key}>
+            {this.props.options[key]}
+          </option>
+        ))}
+      </select>
+    )
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      starWarsValue: Object.keys(starWarsOptions)[0],
+      petValue: Object.keys(petOptions)[0]
+    }
+    this.handleStarWarsChange = this.handleStarWarsChange.bind(this)
+    this.handlePetChange = this.handlePetChange.bind(this)
+  }
+
+  handleStarWarsChange(newValue) {
+    this.setState({
+      starWarsValue: newValue
+    })
+  }
+
+  handlePetChange(newValue) {
+    this.setState({
+      petValue: newValue
+    })
+  }
+
+  render() {
+    // const numbers = [1, 2, 3, 4, 5]
+    // console.log(numbers.map(elem => elem * elem))
+    return (
+      <>
+        {/* <ul>
+          {numbers.map((elem, idx) => <li key={elem * elem}>{elem * elem}</li>)}
+        </ul> */}
+        <form onSubmit={(event) => {
+          event.preventDefault()
+          console.log(`== ${this.state.starWarsValue} ${this.state.petValue}`)
+        }}>
+          <Dropdown
+            options={starWarsOptions}
+            value={this.state.starWarsValue}
+            handleChange={this.handleStarWarsChange} />
+
+          <Dropdown options={petOptions}
+            value={this.state.petValue}
+            handleChange={this.handlePetChange} />
+
+          <button type="submit">Submit</button>
+        </form>
+      </>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 // import App from './HandlingEventsApp'
-import App from './ControlledComponentsApp'
-// import App from './GeneralPurposeSelectApp'
+// import App from './ControlledComponentsApp'
+import App from './GeneralPurposeSelectApp'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
